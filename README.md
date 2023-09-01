84e10466de1cb03ad6229ecfc71f13ce2de9a4a4
Hugo Viana <hugosemianoviana@gmail.com>
2023-09-01 18:09:50 +0200

    feat: improving initial features

--- README.md
+++ README.md
@@ -1,43 +1,52 @@
 # :page_with_curl: JSDebug
 
-## Description
+## :speech_balloon: Description
 
 Here we have a very simple and very basic script to output console log entries to an html element.
 
 The idea was not to develop a "master piece", but only to find a "simple and effective" workaround for the security measures implemented in some  companies (like mine), where they have restricted access to console devops in all web browsers, and therefore we are blind to the live log outputs.
 
-## How to use it
+In the example provided below, I overwrite the console global variable with an `HTMLLogger` instance so that all console entries go straight to the `HTMLLogger`, but you can also case use the `HTMLLogger` as an additional logger to the console log.
+
+For example, if you have a web app that performs certain actions and you want to display to the user all this actions, `HTMLLogger` could come in handy with some minor tweaks.
+
+## :mag_right: How to use it
+
+I'll explain it step by step but you can find a workable example in `test.html` file located in the `tests` folder of this repo.
+
+1. At this moment (maybe i'll improve it later), this package has some parts that require `fontawesome` and `bootstrap`, so you must import it, as well as the `jsdebug.css` style file:
+
+    ```html
+    <head>
+        <link rel="stylesheet" href="../src/jsdebug.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    </head>
+    <body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    </body>
+    ```
 
 1. Create the html element where you'll want to send the console log:
 
     ```html
-    <body>
     <div id="logDiv"></div>
-    </body>
     ```
 
-    It doesn't matter where you locate the div just as long as it's inside the body tag and prior to any script tag.
+    It doesn't matter where you locate the div just as long as it's inside the body tag and prior to any script tag (except the bootstrap script):
 
 1. Place a the following script blocks prior to any other script blocks:
 
     ```html
     <script src="../src/jsdebug.js"></script>
     <script>
+        console.log("Before activating HTMLLogger");
         let logDiv = document.getElementById("logDiv");
         console = new HTMLLogger(logDiv);
+        console.log("After activating HTMLLogger");
     </script>
     ```
 
     :warning: If you have other scripts prior to these 2, all console log entries will go to the standard browser log instead of your html element.
-
-1. (**optional**) If you want to use the out-of-the-box styling, you'll have to import it, as well as a font-awesome style package:
-
-    ```html
-    <head>
-        <link rel="stylesheet" href="../src/jsdebug.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer">
-    </head>
-    ```
 
 The **JSDebug** links provided in this example are local paths. If you want to use the public links, my recommendation is that you use [jsdelivr for GitHub](https://www.jsdelivr.com/github).
 
@@ -45,7 +54,28 @@ For example, to use the latest **JSDebug** available version, use:
 - :link: CSS: https://cdn.jsdelivr.net/gh/hugo-viana/JSdebug@master/src/jsdebug.css
 - :link: JS: https://cdn.jsdelivr.net/gh/hugo-viana/JSdebug@master/src/jsdebug.js
 
-## Limitations
+## :test_tube: Additional options/features
+
+- **Log Filter**
+
+    If you want to be able to filter log entries by log level, you have to create a select element and an additional element (a `div` for instance) and register it into the `HTMLLogger` instance:
+
+    HTML:
+    ```html
+    <body>
+        <select id="selectLogLevels"></select><div id="selectedLogLevels"></div>
+        <div id="logDiv"></div>
+    </body>
+    ```
+
+    Javascript:
+    ```javascript
+    let selectLogLevels = document.getElementById("selectLogLevels");
+    let selectedLogLevels = document.getElementById("selectedLogLevels");
+    console.setLevelsController(selectLogLevels, selectedLogLevels);
+    ```
+
+## :no_entry: Limitations
 
 The standard [console API](https://developer.mozilla.org/en-US/docs/Web/API/console) allows for several additional methods that this package doesn't support (at the moment). The only known methods for this package are:
 - console.log()
@@ -54,6 +84,6 @@ The standard [console API](https://developer.mozilla.org/en-US/docs/Web/API/cons
 - console.info()
 - console.debug()
 
-## Collaboration
+## :bulb: Collaboration
 
 I don't think this is one of the most worthy repositories for you to invest into, but if you have and idea or a new feature proposal, please feel free to open an issue in this repo.
--- src/jsdebug.js
+++ src/jsdebug.js
@@ -1,35 +1,84 @@
+const logLevels = {
+    "error": "Error",
+    "warn": "Warning",
+    "info": "Info",
+    "debug": "Debug",
+    "log": "No level",
+}
+
 class HTMLLogger {
 
-    constructor(html_element) {
-        this.html_element = html_element;
+    constructor(logEntriesContainerElement) {
+        // entries
+        this._entriesContainer = logEntriesContainerElement;
+        this.entriesContainer;
+        this.id = 0;
+        // levelsController
+        this._levelsController;
+        this._selectedLevelsElement;
+        this.selectedLevels = [];
+        for (const level in logLevels) {
+            this.selectedLevels.push(level);
+        }
     }
 
-    _log(content, level=undefined){
+    // Log entries
+
+    _log(content, level="log"){
+
+        if (this.id == 0) {
+            this.entriesContainer = document.createElement("div");
+            this.entriesContainer.classList.add("accordion");
+            this.entriesContainer.classList.add("accordion-flush");
+            this._entriesContainer.appendChild(this.entriesContainer);
+        }
+
         let p = document.createElement("p");
-        p.classList.add("log-entry");
         p.classList.add("log-" + level);
-        let html;
+        p.classList.add("m-1");
+        p.classList.add("font-monospace");
+        let faIcon;
         switch (level) {
             case "error":
-                html = `<i class="fa-solid fa-circle-xmark"></i>`;
+                faIcon = "fa-circle-xmark";
                 break;
             case "warn":
-                html = `<i class="fa-solid fa-triangle-exclamation"></i>`;
+                faIcon = "fa-triangle-exclamation";
                 break;
             case "info":
-                html = `<i class="fa-solid fa-circle-info"></i>`;
+                faIcon = "fa-circle-info";
                 break;
             case "debug":
-                html = `<i class="fa-solid fa-code"></i>`;
+                faIcon = "fa-code";
                 break;
             default:
-                html = `<i class="fa-solid fa-info"></i>`;
+                faIcon = "fa-comment";
         }
-        p.innerHTML = html;
-        let span = document.createElement("span");
-        span.innerHTML = "&nbsp;" + content;
-        p.appendChild(span);
-        this.html_element.appendChild(p);
+
+        if (this.selectedLevels.indexOf(level) == -1) {
+            p.classList.add("d-none");
+        }
+
+        let levelString = logLevels[level];
+        if (level == "log") {
+            levelString = "";
+        }
+
+        p.innerHTML = `
+        <div class="accordion-item">
+            <div class="accordion-header ">
+                <button class="accordion-button collapsed p-0 log-entry log-${level}" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse-${this.id}" aria-expanded="false" aria-controls="flush-collapse-${this.id}">
+                    <i class="fa-solid ${faIcon}"></i>&nbsp;${levelString}&nbsp;[@${this._getStack()}]
+                </button>
+            </div>
+                <div id="flush-collapse-${this.id}" class="accordion-collapse collapse">
+                <div class="accordion-body log-entry log-${level} text-break">${content}</div>
+            </div>
+        </div>
+        `;
+
+        this.entriesContainer.appendChild(p);
+        this.id += 1;
     }
 
     error(content) {
@@ -46,6 +95,119 @@ class HTMLLogger {
     }
     log(content) {
         this._log(content);
+    }
+
+    _getStack() {
+        let stack;
+        try {
+            throw new Error();
+        } catch(e) {
+            stack = e.stack;
+        }
+        stack = stack.split(" at ")[4];  // get caller
+
+        let _x = stack.split("/");
+        stack = stack.split("/")[_x.length-1]; // get file:line:chr
+
+        return stack;
+    }
+
+    // logLevels controller
+
+    get hasLevelsController() {
+        if (this._levelsController) {
+            return true;
+        }
+        return false;
+    }
+
+    setLevelsController(levelsControllerElement, selectedLevelsElement) {
+        this._levelsController = levelsControllerElement;
+        this._levelsController.logger= this;
+        this._selectedLevelsElement = selectedLevelsElement;
+        this.initLevelsController();
+    }
+
+    initLevelsController() {
+        if (this.hasLevelsController === false) {
+            console.debug("Cannot initialize HTMLLogger levelsController because no levelsControllerElement provided.");
+            return;
+        }
+        console.debug("Initializing HTMLLogger levelsController.");
+        // empty log levels options
+        for(var i = (this._levelsController.options.length - 1); i >= 0; i--) {
+            this._levelsController.remove(i);
+        }
+        // load levels list
+        let opt = document.createElement("option");
+        opt.value = 0;
+        opt.innerHTML = "All";
+        opt.classList.add("dropdown-item");
+        this._levelsController.appendChild(opt);
+        let hr = document.createElement("hr");
+        hr.classList.add("dropdown-divider");
+        this._levelsController.appendChild(hr);
+        for(var i = 0; i < this.selectedLevels.length; i++) {
+            opt = document.createElement("option");
+            opt.value = this.selectedLevels[i];
+            opt.innerHTML = logLevels[this.selectedLevels[i]];
+            opt.classList.add("dropdown-item");
+            this._levelsController.appendChild(opt);
+        }
+        // reset text to All
+        this._selectedLevelsElement.innerHTML = "All";
+        // activate filtering
+        this._levelsController.addEventListener(
+            "change",
+            function () {
+                this.logger.filterLogLevels();
+              },
+        );
+    }
+
+    filterLogLevels() {
+
+        let showLevels = [];
+
+        for(var i = 0; i < this._levelsController.options.length; i++) {
+            if (this._levelsController.options[i].selected) {
+                showLevels.push(this._levelsController.options[i].value);
+            }
+        }
+        this.selectedLevels = showLevels;
+
+        let showLevelsString = "";
+        for(var i = 0; i < showLevels.length; i++) {
+            if (showLevels[i] == 0) {
+                showLevelsString = "All";
+                showLevels = [];
+                for (const level in logLevels) {
+                    showLevels.push(level);
+                }
+                break;
+            }
+            if (i > 0) {
+                showLevelsString += ", "
+            }
+            showLevelsString += logLevels[showLevels[i]];
+        }
+
+        this._selectedLevelsElement.innerHTML = showLevelsString;
+
+        for(var i = 0; i < this.entriesContainer.children.length; i++) {
+            let hide = true;
+            for(var n = 0; n < showLevels.length; n++) {
+                if (this.entriesContainer.children[i].classList.contains("log-" + showLevels[n])) {
+                    hide = false;
+                }
+            }
+            if (hide === false) {
+                this.entriesContainer.children[i].classList.remove("d-none");
+            } else {
+                this.entriesContainer.children[i].classList.add("d-none");
+            }
+        }
+
     }
 
 }
